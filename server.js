4b80adc6570222c8a252e1713b72ea91a180dc45
LukasKub <119612062+LukasKub@users.noreply.github.com>
2026-08-02 13:12:53 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "/rss");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.listen(port, () =>
+    console.log("Listening on port " + port + "..."));
+
+// Set up static file serving
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Node.js middleware for serving a favicon
+var favicon = require('serve-favicon');
+app.use(favicon(__dirname + '/favicon.ico'));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html",);
+});
+
+app.get("/forecast", function (req, res) {
+    res.sendFile(__dirname + "/index.html",);
+});
+
+app.get('/rss', (req, res) => {
+    try {
+        const RSS_URL = `https://www.in-pocasi.cz/rss.php`;
+
+        function parseRSS(rssText) {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(rssText, 'application/xml');
+            return xmlDoc;
+        }
+
+        fetch(RSS_URL)
+            .then(response => response.text())
+            .then(data =>
+                res.json({ message: data }))
+
+    } catch (error) {
+        console.error('Error reading files:', error);
+    }
+});
